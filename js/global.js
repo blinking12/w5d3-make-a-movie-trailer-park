871b9e769e0c4ee01feee63187ee1e88156c1ec2
Troy Denney <tvdenney@gmail.com>
2016-10-26 15:44:25 -0400

    Spaceships

--- js/global.js
+++ js/global.js
@@ -19,3 +19,61 @@ var trailer1 = new Trailer('about our marriage...', '-8NM2o90oMM')
 var trailer2 = new Trailer('the vlog is cancelled', 'V4PsrOonXic')
 var trailer3 = new Trailer('something i\'ve been holding back', 'gx3fH2-FiCo')
 var trailer4 = new Trailer('what\'s most important', 'S7HxFg3HWOg')
+
+
+//Spaceship portion of the assignment.
+
+// Make an object named spaceship1 using a JavaScript object literal. Include properties with this information: - Top speed of 2lyph (light years per hour) - Ship name of Galaxy Cruiser
+// Include a method (a property that is set to an anonymous function) named launch that console logs your ship name + ' launching to infinity and beyond at ' + top speed + '!'. After you create your spaceship object, call the launch method to see the console log.
+// Make three other spaceships with different speeds and names using the three other ways of making objects seen in the examples above.
+
+// Object Literal
+var spaceship1 = {
+    topSpeed: 'lyph',
+    name: 'Galaxy Cruiser',
+    fly: function(speed) {
+        console.log(this.name + ' launching to infinity and beyond at ' + speed + this.topSpeed + '!')
+    }
+}
+spaceship1.fly(42)
+
+
+// // Object New
+var spaceship2 = new Object()
+spaceship2.name = 'Galaxy Cruiser'
+spaceship2.topSpeed = 'lyph'
+spaceship2.fly = function(speed) {
+    console.log(this.name + ' launching to infinity and beyond at ' + speed + this.topSpeed + '!')
+}
+
+spaceship2.fly(35)
+
+// Constructor Function
+var Spaceship3 = function() {
+    this.name = 'Galaxy Cruiser'
+    this.topSpeed = 'lyph'
+    this.fly = function(speed) {
+        console.log(this.name + ' launching to infinity and beyond at ' + speed + this.topSpeed + '!')
+    }
+}
+
+var ship = new Spaceship3()
+
+ship.fly(85)
+
+
+// Class
+class Spaceship4 {
+    constructor() {
+        this.name = 'Galaxy Cruiser'
+        this.topSpeed = 'lyph'
+    }
+
+    fly(speed) {
+        console.log(this.name + ' launching to infinity and beyond at ' + speed + this.topSpeed + '!')
+    }
+}
+
+var ship = new Spaceship4()
+
+ship.fly(55)
